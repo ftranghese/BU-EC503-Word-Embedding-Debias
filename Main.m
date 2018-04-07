a443bb7395b61bf57546e9460aa7fe53fb711cf2
Nidhi T <nidhit@bu.edu>
2018-04-07 18:09:08 -0400

    adding gender direction

--- Main.m
+++ Main.m
@@ -3,7 +3,28 @@
     March 2018
     Word Embeddings De-biasing
     Main script
-    Word by: Frank Tranghese, Nidhi Tiwari, Aditya Singh
+    Work by: Frank Tranghese, Nidhi Tiwari, Aditya Singh
 %}
 
 % CODE GOES HERE
+num_dataFiles = 1;
+words = [];
+wordVectors = [];
+% Loading dataset from each matfile into different lists containing words and wordvectors
+for i = 1:num_dataFiles
+    load(sprintf('model_part_%02d.mat',i));
+    words = [words, words_part];
+    wordVectors = [wordVectors; wordvecs_part];
+end
+
+wordIndex = containers.Map(words, (1:length(words)));
+% Normalising the word vectors
+normalised_wordVecs = zeroes(size(wordVectors,1),1);
+for j = 1:size(wordVectors,1)
+    vector = wordVectors(j,:);
+    % Normalising
+    normalised_wordVecs(j,:) = vector./norm(vector);
+end
+
+
+ 
